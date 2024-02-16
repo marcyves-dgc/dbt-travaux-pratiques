8c5bfc9dacd9213c939ebe6fd2948b8572e1249e
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2024-02-16 16:27:05 +0100

    feat: first_init
    **Features**
    - This is my first feature.
    - This is my second feature.
    - ...
    
    **Fixes**
    - `Operator` is now fixed.
    
    **Breaking Changes**
    /
    
    Issue #

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install dbt
+echo -e "\n### INSTALLING dbt ###\n"
+pip install dbt
+
+
+echo -e "\n ### INSTALLING DBT-BIG QUERY CONNECTION ###\n"
+pip install dbt-bigquery
+
+# Create a new project
+echo -e "\n### CLONING dbt PROJET PROJECT ###\n"
+git clone https://github.com/dbt-labs/jaffle_shop.git
+
+echo -e "\n### SETTING UP USER PROFILE ###\n"
+
+read -p "Sandbox name: (no need to write 'sandbox' in front)" sandbox_name
+
+read -p "Dataset name: ('TP_dbt__' in front)" dataset_name
+
+additional_text="
+jaffle_shop:
+  outputs:
+    dev:
+      dataset: TP_dbt__${dataset_name}
+      job_execution_timeout_seconds: 300
+      job_retries: 1
+      location: EU
+      method: oauth
+      priority: interactive
+      project: sandbox-${sandbox_name}
+      threads: 1
+      type: bigquery
+  target: dev
+"
+
+echo "$additional_text" >> ~/.dbt/profiles.yml
+
+echo -e "\n### SETUP COMPLETE ###\n"
