af89b0c40e0f143fc2bb7410db8625fd4b9b2483
vvaneeclo <150353871+vvaneeclo@users.noreply.github.com>
2024-02-26 16:13:44 +0100

    Update setup.sh
    Deleted all profiles.yml handling since dbt is already working.

--- setup.sh
+++ setup.sh
@@ -14,28 +14,4 @@ python3 -m pip install dbt-bigquery
 echo -e "\n### CLONING dbt PROJET PROJECT ###\n"
 git clone https://github.com/dbt-labs/jaffle_shop.git
 
-echo -e "\n### SETTING UP USER PROFILE ###\n"
-
-read -p "Sandbox name (no need to write 'sandbox' in front): " sandbox_name
-
-read -p "Dataset name ('TP_dbt__' in front): " dataset_name
-
-profile="
-jaffle_shop:
-  outputs:
-    dev:
-      dataset: TP_dbt__${dataset_name}
-      job_execution_timeout_seconds: 300
-      job_retries: 1
-      location: EU
-      method: oauth
-      priority: interactive
-      project: sandbox-${sandbox_name}
-      threads: 1
-      type: bigquery
-  target: dev
-"
-
-echo "$profile" >> ~/.dbt/profiles.yml
-
 echo -e "\n### SETUP COMPLETE ###\n"
