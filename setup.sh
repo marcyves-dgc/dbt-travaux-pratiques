cf35fd8c2740bf0c20c487803f03a4935f5981fc
vvaneeclo <150353871+vvaneeclo@users.noreply.github.com>
2024-02-27 16:11:14 +0100

    Added colors.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 python3 -m venv dbt-env
+source dbt-env/bin/activate
 
-# Install dbt
-echo -e "\n### INSTALLING dbt ###\n"
+BOLD_RED='\033[1;31m'
+BOLD_GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "\n${BOLD_GREEN}### INSTALLING dbt ###${NC}\n"
 python3 -m pip install dbt
 
-
-echo -e "\n ### INSTALLING DBT-BIG QUERY CONNECTION ###\n"
+echo -e "\n${BOLD_GREEN}### INSTALLING DBT-BIG QUERY CONNECTION ###${NC}\n"
 python3 -m pip install dbt-bigquery
 
 # Create a new project
-echo -e "\n### CLONING dbt PROJET PROJECT ###\n"
+echo -e "\n${BOLD_GREEN}### CLONING dbt PROJET PROJECT ###${NC}\n"
 git clone https://github.com/dbt-labs/jaffle_shop.git
 
-echo -e "\n### ACTIVATING VIRTUAL ENV ###\n"
+echo -e "\n${BOLD_GREEN}### SETUP COMPLETE ###${NC}\n"
 
-source "dbt-env/bin/activate"
+echo -e "\n### PLEASE COPY-PASTE ${BOLD_RED}source dbt-env/bin/activate${NC} IN THE TERMINAL ###\n"  
 
-echo -e "\n### SETUP COMPLETE ###\n"
+echo -e "\n### THEN copy-paste ${BOLD_RED}cd jaffle_shop${NC} & then ${BOLD_RED}dbt init${NC} ###\n"
